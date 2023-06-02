282a04a8084bc9b02d66d61f5417878caa9f6709
abeeththebest <134381005+abeeththebest@users.noreply.github.com>
2023-06-02 11:17:02 +0530

    Update and rename circle.py to loopcircle.py

--- loopcircle.py
+++ loopcircle.py
@@ -1,18 +1,18 @@
 import turtle
 
 from itertools import cycle
-colors = cycle(['gold', 'red', 'pink', 'blue', 'blue', 'navy'])
+colors = cycle(['gold', 'red', 'pink', 'blue', 'maroon', 'navy'])
 
 
 def draw_circle(size,angle,shift):
-    turtle.bgcolor(next(colors))
+    turtle.bgcolor("black")
     turtle.pencolor(next(colors))
     turtle.circle(size)
     turtle.right(angle)
     turtle.forward(shift)
-    draw_circle(size+5, angle+1,shift+1)
+    draw_circle(size+7, angle+0,shift+0)
 
 turtle.bgcolor('black')
 turtle.speed('fast')
-turtle.pensize(40)
-draw_circle(30,0,1)
+turtle.pensize(3)
+draw_circle(10,0,0)
